76422d4a2a0f510514cfefe47de0a505ac4579ab
strider76 <manuel@betangible.com>
2016-09-28 17:17:03 +0200

    Creamos clase Persona con los atributos iniciales

---
+++ Documents/NetBeansProjects/EjemplosVarios/EjemplosVarios/src/ejemplosvarios/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosvarios;
+
+/**
+ *
+ * @author strider
+ */
+public class Persona {
+    
+    private String nombre;
+    private int edad;
+    private String dni;
+    private char sexo; // H -> hombre, M-> Mujer
+    private int peso; // en Kg
+    private int altura; // en cms
+    
+    
+}
