f118c2a8ab80ffa9ec94bd6fe2836102702309f4
strider76 <manuel@betangible.com>
2016-09-29 09:41:14 +0200

    EjemplosVarios ejecutando la Clase Persona ya terminada

--- Documents/NetBeansProjects/EjemplosVarios/EjemplosVarios/src/ejemplosvarios/EjemplosVarios.java
+++ Documents/NetBeansProjects/EjemplosVarios/EjemplosVarios/src/ejemplosvarios/EjemplosVarios.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ejemplosvarios;
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +17,50 @@ public class EjemplosVarios {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String nombre;
+        int edad;
+        int peso;
+        int altura;
+        char sexo;
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Introduzca Nombre: ");
+        nombre=sc.nextLine();
+        System.out.println("Introduzca Edad: ");
+        edad=sc.nextInt();
+        System.out.println("Introduzca Peso: ");
+        peso=sc.nextInt();   
+        System.out.println("Introduzca Altuta: ");
+        altura=sc.nextInt();
+        System.out.println("Introduzca Sexo (H/M): ");
+        sexo=sc.next().charAt(0);  
+        
+        Persona persona1,persona2,persona3;
+        persona1=new Persona(nombre,edad,sexo,peso,altura);
+        persona2=new Persona(nombre,edad,sexo);
+        persona3=new Persona();
+        
+        persona2.setPeso(peso);
+        persona2.setAltura(altura);
+        
+        persona3.setNombre(nombre);
+        persona3.setAltura(altura);
+        persona3.setEdad(edad);
+        persona3.setPeso(peso);
+        
+        System.out.println("Informacion PERSONA1");
+        System.out.println(persona1.toString());
+        
+        System.out.println("Informacion PERSONA2");
+        System.out.println(persona2.toString());
+        
+        System.out.println("Informacion PERSONA3");
+        System.out.println(persona3.toString());
+        
+        System.out.println("La Persona1 es "+ ((persona1.esMayorDeEdad())?"mayor":"menor")+" de edad");
+        System.out.println("La Persona2 es "+ ((persona2.esMayorDeEdad())?"mayor":"menor")+" de edad");
+        System.out.println("La Persona3 es "+ ((persona3.esMayorDeEdad())?"mayor":"menor")+" de edad");
+        
     }
     
 }
