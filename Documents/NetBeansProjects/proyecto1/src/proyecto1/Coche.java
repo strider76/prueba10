ad9f646ac47c9d0dd203d3b70516d666e4225617
strider76 <manuel@betangible.com>
2016-10-04 12:48:06 +0200

    creacion clase coche y constructor

---
+++ Documents/NetBeansProjects/proyecto1/src/proyecto1/Coche.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+/**
+ *
+ * @author strider
+ */
+public class Coche {
+    
+    private String matricula;
+    
+    public Coche(String matricula){
+        this.matricula=matricula;
+    }
+    
+    
+}
